01f587909157904f04e52e84e636fc708abe324f
Jorge Anzola <jorgeanzola@gmail.com>
2025-02-05 19:26:44 +0100

    Create cp-field-inspect.php

---
+++ src/translations/es/cp-field-inspect.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'CP Field Inspect plugin loaded' => 'CP Field Inspect plugin cargado',
+    'Edit category group' => 'Editar grupo de categoría',
+    'Edit entry type' => 'Editar tipo de entrada',
+    'Edit field settings' => 'Editar configuración del campo',
+    'Edit global set' => 'Editar conjunto global',
+    'Edit product type' => 'Edit tipo de producto',
+    'Edit users settings' => 'Edit configuración de usuarios',
+    'Edit volume' => 'Editar volume',
+];
